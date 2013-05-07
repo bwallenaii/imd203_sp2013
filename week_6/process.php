dfeb50fd114bc6e0fcd8db12fb1c3698ad61c74c
Brent Allen <bwallen.aii@gmail.com>
2013-05-07 03:04:49 +0000

    week 6

---
+++ week_6/process.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Thank you!</title>
+</head>
+<body>
+	<h2>Thanks!</h2>
+	<p>All your private information will be processed and we will
+		match you with someone you will love!!! We'll probably send an email
+		or something. I dunno. We don't have that big a selection right now. Maybe 
+		try back later.</p>
+</body>
+</html>
